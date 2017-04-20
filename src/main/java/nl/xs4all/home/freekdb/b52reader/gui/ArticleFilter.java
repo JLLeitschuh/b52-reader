f0a072cb8a380e190f26ba50890c3b2187cac43c
Freek de Bruijn <freekdb@gmail.com>
2017-04-20 21:36:54 +0200

    Created several constants in the ArticleFilter class.

--- src/main/java/nl/xs4all/home/freekdb/b52reader/gui/ArticleFilter.java
+++ src/main/java/nl/xs4all/home/freekdb/b52reader/gui/ArticleFilter.java
@@ -13,6 +13,15 @@ import nl.xs4all.home.freekdb.b52reader.model.Article;
 import nl.xs4all.home.freekdb.b52reader.utilities.Utilities;
 
 public class ArticleFilter implements Predicate<Article> {
+    private static final String AUTHOR_PREFIX = "author:";
+    private final String TITLE_PREFIX = "title:";
+    private final String IS_PREFIX = "is:";
+
+    private final String STARRED_STATE = "starred";
+    private final String UNSTARRED_STATE = "unstarred";
+    private final String READ_STATE = "read";
+    private final String UNREAD_STATE = "unread";
+
     private String normalizedAuthorName;
     private String normalizedTitle;
     private Boolean starred;
@@ -20,18 +29,18 @@ public class ArticleFilter implements Predicate<Article> {
 
     ArticleFilter(String filterText) {
         Arrays.stream(filterText.split(" ")).forEach(filterPart -> {
-            if (filterPart.startsWith("from:") && filterPart.length() > 5) {
-                normalizedAuthorName = Utilities.normalize(filterPart.substring(5));
+            if (filterPart.startsWith(AUTHOR_PREFIX) && filterPart.length() > AUTHOR_PREFIX.length()) {
+                normalizedAuthorName = Utilities.normalize(filterPart.substring(AUTHOR_PREFIX.length()));
                 System.out.println("Filter on author: " + normalizedAuthorName);
-            } else if (filterPart.startsWith("title:") && filterPart.length() > 6) {
-                normalizedTitle = Utilities.normalize(filterPart.substring(6));
+            } else if (filterPart.startsWith(TITLE_PREFIX) && filterPart.length() > TITLE_PREFIX.length()) {
+                normalizedTitle = Utilities.normalize(filterPart.substring(TITLE_PREFIX.length()));
                 System.out.println("Filter on title: " + normalizedTitle);
-            } else if (filterPart.startsWith("is:") && filterPart.length() > 3) {
-                String state = filterPart.substring(3).toLowerCase();
-                if ("starred".equals(state) || "unstarred".equals(state))
-                    starred = "starred".equals(state);
-                if (state.contains("read") || "unread".equals(state))
-                    read = "read".equals(state);
+            } else if (filterPart.startsWith(IS_PREFIX) && filterPart.length() > IS_PREFIX.length()) {
+                String state = filterPart.substring(IS_PREFIX.length()).toLowerCase();
+                if (STARRED_STATE.equals(state) || UNSTARRED_STATE.equals(state))
+                    starred = STARRED_STATE.equals(state);
+                if (state.equals(READ_STATE) || UNREAD_STATE.equals(state))
+                    read = READ_STATE.equals(state);
                 System.out.println("Filter on state: " + state);
             } else if (!"".equals(filterPart.trim())) {
                 // Incomplete filter parts will be ignored.
