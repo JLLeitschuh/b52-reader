4d52bc188f7c995f408e9f830d6453ccf7ef5b01
Freek de Bruijn <freekdb@gmail.com>
2017-05-25 21:38:35 +0200

    Added Javadocs to the EmbeddedBrowserType class.

--- src/main/java/nl/xs4all/home/freekdb/b52reader/general/EmbeddedBrowserType.java
+++ src/main/java/nl/xs4all/home/freekdb/b52reader/general/EmbeddedBrowserType.java
@@ -6,7 +6,17 @@
 
 package nl.xs4all.home.freekdb.b52reader.general;
 
+/**
+ * The different options that are available for embedded browsers.
+ */
 public enum EmbeddedBrowserType {
+    /**
+     * The {@link chrriis.dj.nativeswing.swtimpl.components.JWebBrowser} from the DJ Native Swing project.
+     */
     EMBEDDED_BROWSER_DJ_NATIVE_SWING,
+
+    /**
+     * A simple placeholder for the real embedded browsers (a panel with a label).
+     */
     EMBEDDED_BROWSER_PLACEHOLDER
 }
